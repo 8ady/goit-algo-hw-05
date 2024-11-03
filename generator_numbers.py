d993d8f6d1035f16d0602df25c61421c8a6831bd
Denys Mykhailov <mykhailovdenis@gmail.com>
2024-11-03 18:08:33 +0000

    Added 5th HM

--- generator_numbers.py
+++ generator_numbers.py
@@ -12,7 +12,3 @@ def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
-
-from typing import Callable, Dict
-
-contacts = {}
